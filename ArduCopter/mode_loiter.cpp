8f1a2d9ad2d0a4d44ffa0057ef5278f3d9ce431e
hong <70960247+rutile-Hong@users.noreply.github.com>
2024-10-18 10:13:32 -0500

    Update mode_loiter.cpp

--- ArduCopter/mode_loiter.cpp
+++ ArduCopter/mode_loiter.cpp
@@ -33,7 +33,7 @@ bool ModeLoiter::init(bool ignore_checks)
     // set vertical speed and acceleration limits
     pos_control->set_max_speed_accel_z(-get_pilot_speed_dn(), g.pilot_speed_up, g.pilot_accel_z);
     pos_control->set_correction_speed_accel_z(-get_pilot_speed_dn(), g.pilot_speed_up, g.pilot_accel_z);
-
+    
 #if AC_PRECLAND_ENABLED
     _precision_loiter_active = false;
 #endif
@@ -89,7 +89,7 @@ void ModeLoiter::run()
 
     // set vertical speed and acceleration limits
     pos_control->set_max_speed_accel_z(-get_pilot_speed_dn(), g.pilot_speed_up, g.pilot_accel_z);
-
+    
     // process pilot inputs unless we are in radio failsafe
     if (!copter.failsafe.radio) {
         // apply SIMPLE mode transform to pilot inputs
@@ -191,8 +191,10 @@ void ModeLoiter::run()
         // get avoidance adjusted climb rate
         target_climb_rate = get_avoidance_adjusted_climbrate(target_climb_rate);
 
+#if AP_RANGEFINDER_ENABLED
         // update the vertical offset based on the surface measurement
         copter.surface_tracking.update_surface_offset();
+#endif
 
         // Send the commanded climb rate to the position controller
         pos_control->set_pos_target_z_from_climb_rate_cm(target_climb_rate);
